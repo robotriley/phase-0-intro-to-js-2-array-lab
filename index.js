c6e735adc12141f554cd4acfe7686f39cbffcde1
riley soloner <rileysoloner@gmail.com>
2022-06-08 10:23:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    };
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+};
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+};
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+};
+
+function prependCat(name) {
+    const newArray = [name, ...cats];
+    return newArray;
+};
+
+function removeLastCat(name) {
+    const newArray = cats.slice(0, -1);
+    return newArray;
+};
+
+function removeFirstCat(name) {
+    const newArray = cats.slice(1);
+    return newArray;
+};
